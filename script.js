1e6ec7e82d8d094878a8c82bab8a79e5d95fdfc9
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-10-28 18:40:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,75 @@
-// Clears the calculator display
-
-function clearScreen() {
-
-document.getElementById("result").value = "";
-
+function getHistory() {
+  return document.getElementById("history-value").innerText;
 }
-
-// Appends the clicked button's value to the display
-
-function setScreenValue(value) {
-
-document.getElementById("result").value += value;
-
+function printHistory(num) {
+  document.getElementById("history-value").innerText = num;
 }
-
-// Calculates and displays the result
-
-function calculateResult() {
-
-const resultElement = document.getElementById("result");
-
-const expression = resultElement.value.trim();
-
-// Check for empty input
-
-if (expression === "") {
-
-resultElement.value = "Enter an expression";
-
-return;
-
+function getOutput() {
+  return document.getElementById("output-value").innerText;
 }
-
-// Evaluate expression and handle errors
-
-try {
-resultElement.value = eval(expression);
-} catch (e) {
-resultElement.value = "Invalid expression";
+function printOutput(num) {
+  if (num == "") {
+    document.getElementById("output-value").innerText = num;
+  } else {
+    document.getElementById("output-value").innerText = getFormattedNumber(num);
+  }
 }
+function getFormattedNumber(num) {
+  if (num == "-") {
+    return "";
+  }
+  var n = Number(num);
+  var value = n.toLocaleString("en");
+  return value;
+}
+function reverseNumberFormat(num) {
+  return Number(num.replace(/,/g, ""));
+}
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    if (this.id == "clear") {
+      printHistory("");
+      printOutput("");
+    } else if (this.id == "backspace") {
+      var output = reverseNumberFormat(getOutput()).toString();
+      if (output) {
+        //if output has a value
+        output = output.substr(0, output.length - 1);
+        printOutput(output);
+      }
+    } else {
+      var output = getOutput();
+      var history = getHistory();
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substr(0, history.length - 1);
+        }
+      }
+      if (output != "" || history != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        history = history + output;
+        if (this.id == "=") {
+          var result = eval(history);
+          printOutput(result);
+          printHistory("");
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printOutput("");
+        }
+      }
+    }
+  });
+}
+var number = document.getElementsByClassName("number");
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    var output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      //if output is a number
+      output = output + this.id;
+      printOutput(output);
+    }
+  });
 }
