f375518b4a16fc07546ce413cdc42f8bdf123230
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-11-04 18:31:58 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,12 @@
+f_n = "arnav pal"
+l_n = "choudhury"
+ful_n = f_n + l_n
+x = "hi" * 5
+print("first name", f_n)
+print("last name", l_n)
+print("full name", ful_n)
+print("string multiplied 5 times", x)
+n = "arnav pal choudhury"
+print(len(n))
+print(n[0])
+print(n[0:3])
