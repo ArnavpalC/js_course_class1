b186745a3c97386a839118b23f7a50ad98ccf358
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-11-04 18:19:02 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+name = "arnav"
+age = 12
+student = True
+weight = 47.8
+
+print("data type of name is", type(name),name)
+print("data type of age is", type(age),age)
+print("data type of student is", type(student),student)
+print("data type of weight is", type(weight),weight)
