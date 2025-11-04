be5e7f89264d354af42a770a30687a7d39de3355
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-11-04 18:25:03 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,13 @@
+num1 = 45
+num2 = 3
+print("num1", num1)
+print("num2", num2)
+print("addition", num1 + num2)
+print("subtraction", num1 - num2)
+print("multiplication", num1 * num2)
+print("division", num1 / num2)
+print("floor division", num1 // num2)
+print("expodential", num1 ** num2)
+print("equal to", num1 == num2)
+print("lesser then", num1 < num2)
+print("greater then", num1 > num2)
