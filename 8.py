b550b92035844567416a20a9244cae55b7c7eae0
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-11-17 18:30:04 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,9 @@
+n = int(input("enter number to check: "))
+if n>50:
+    print("number is greater then 50")
+    if n%2==0:
+        print("it is an even number")
+    else :
+        print("its an odd number")
+else :
+    print("number is less then 50")
