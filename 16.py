06de6beb2776398424f008ca2384b86689795efa
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-11-24 18:37:13 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+num1 = int(input('enter number 1: '))
+num2 = int(input('enter number 2: '))
+
+print("sum: ", add(num1,num2))
+print("difference: ", sub(num1,num2))
+print("multiply: ", multiply(num1,num2))
+print("division: ", divide(num1,num2))
+
